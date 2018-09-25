3cce271dca6c63e4886f8e7bb4fe5148f8e26861
ricglz0201 <rjgcastillo@hotmail.com>
2018-09-25 13:41:07 -0500

    Add new text and change link of

--- src/SinglePlayer/SPContent.jsx
+++ src/SinglePlayer/SPContent.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import {firstParagraph, secondParagraph, thirdParagraph} from './SPText'
+import {firstParagraph, secondParagraph, thirdParagraph, fourthParagraph} from './SPText'
 import SPLinks from './SPLinks'
 import SPCarousel from './SPCarousel'
 
@@ -34,6 +34,7 @@ class SPContent extends React.Component {
             <Typography variant='body1' component='p'>{firstParagraph}</Typography>
             <Typography variant='body1' component='p'>{secondParagraph}</Typography>
             <Typography variant='body1' component='p'>{thirdParagraph}</Typography>
+            <Typography variant='body1' component='p'>{fourthParagraph}</Typography>
             <SPLinks/>
           </Grid>
           <Grid item xs={3}>
