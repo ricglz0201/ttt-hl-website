6a99d905ae1830c16005236885d35895a49127e0
Ricardo J. González C <rjgcastillo@hotmail.com>
2020-04-28 11:03:37 -0500

    Fix lint issue

--- src/Helper/Paragraphs.jsx
+++ src/Helper/Paragraphs.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 
 const Paragraphs = ({ paragraphs }) => (
-  paragraphs.map(paragraph => (
+  paragraphs.map((paragraph) => (
     <Typography key={paragraph} variant="body1" component="p">
       {paragraph}
     </Typography>
